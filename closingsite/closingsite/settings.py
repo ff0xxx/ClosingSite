8012bbdfd2620d0da7227c6eb148c790515640ba
ff0xxx <poisk8302@email.com>
2024-09-13 17:52:18 +0300

    +postgreSQL, +cool search

--- closingsite/closingsite/settings.py
+++ closingsite/closingsite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     'debug_toolbar',
     
@@ -82,8 +83,12 @@ WSGI_APPLICATION = 'closingsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clothing_db',
+        'USER': 'sitewomen',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
